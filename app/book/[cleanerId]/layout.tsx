3802bdb59c7c34f59580ffd2507fc6e6f1ac4b42
Atharva Manjunath <atharvam@Atharvas-MacBook-Air.local>
2025-06-28 00:48:34 -0700

    fix: remove invalid params from layout

--- app/book/[cleanerId]/layout.tsx
+++ app/book/[cleanerId]/layout.tsx
@@ -1,17 +1,10 @@
 // app/book/[cleanerId]/layout.tsx
 import { ReactNode } from 'react'
 
-export default function CleanerBookingLayout({
-  children,
-  params,
-}: {
+interface LayoutProps {
   children: ReactNode
-  params: { cleanerId: string }
-}) {
-  return (
-    <div>
-      {/* You can use params.cleanerId if needed */}
-      {children}
-    </div>
-  )
+}
+
+export default function Layout({ children }: LayoutProps) {
+  return <>{children}</>
 }
